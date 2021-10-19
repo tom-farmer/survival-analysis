080f90cfb4034484d6ba142057d62c2d8dd12f3c
Thomas Farmer <thomasfarmer@Thomass-MacBook-Pro.local>
2021-10-19 15:16:15 -0500

    updated folder name and gitignore

--- run_rstudio.sh
+++ run_rstudio.sh
@@ -2,5 +2,5 @@ docker run -d \
 	-p 8787:8787 \
 	-e DISABLE_AUTH=true \
 	--mount type=bind,source="$(pwd)",target=/code \
-	--name survival_analysis \
+	--name survival-analysis \
 	rocker/tidyverse:4.0.5
